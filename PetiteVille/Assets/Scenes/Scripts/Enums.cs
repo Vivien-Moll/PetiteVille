57c70119cb073739a09b22b1788b2ef99866f8b9
Vivien-Moll <82842144+Vivien-Moll@users.noreply.github.com>
2021-11-23 13:16:49 +0100

    lolmdr

---
+++ PetiteVille/Assets/Scenes/Scripts/Enums.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Tetris
+{
+    Square,
+    Line,
+    T,
+    L,
+    L_Inverted,
+    S,
+    S_Inverted
+}
+
+public enum Tile
+{
+    Empty,
+    House,
+    Road,
+    River,
+    Park,
+    Factory
+}
